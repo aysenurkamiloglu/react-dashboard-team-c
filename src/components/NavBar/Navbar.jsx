de980379d7a0a4f63ab510090fd3cd151c19378e
Utkan <41093352+utk4n@users.noreply.github.com>
2022-07-13 22:42:02 +0300

    Update Navbar.jsx

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,13 +1,13 @@
-import Logout from './Logout'
-import Tab from '../NavBar/Tab'
-import style from '../NavBar/navbar.module.css'
-import dashboard from './img/dashboard.svg'
-import members from './img/members.svg'
-import modules from './img/modules.svg'
-import projects from './img/projects.svg'
-import sprint from './img/sprint.svg'
-import report from './img/report.svg'
-import collapse from './img/collapse.svg'
+import Logout from "./Logout";
+import Tab from "../NavBar/Tab";
+import style from "../NavBar/navbar.module.css";
+import dashboard from "./img/dashboard.svg";
+import members from "./img/members.svg";
+import modules from "./img/modules.svg";
+import projects from "./img/projects.svg";
+import sprint from "./img/sprint.svg";
+import report from "./img/report.svg";
+import collapse from "./img/collapse.svg";
 
 const Navbar = () => {
     return (
@@ -18,17 +18,51 @@ const Navbar = () => {
                     <img className={style.collapse} src={collapse} />
                 </div>
                 <Tab activeTab={1}>
-                    <Tab.Panel>  <div><img src={dashboard}></img>Dashboard</div></Tab.Panel>
-                    <Tab.Panel> <div className={style.active}><img src={projects}></img>Projects</div></Tab.Panel>
-                    <Tab.Panel><div><img src={modules}></img>Modules</div></Tab.Panel>
-                    <Tab.Panel> <div><img src={sprint}></img>Sprint</div></Tab.Panel>
-                    <Tab.Panel><div><img src={members}></img>Members</div></Tab.Panel>
-                    <Tab.Panel> <div><img src={report}></img>Reports</div></Tab.Panel>
+                    <Tab.Panel>
+                        {" "}
+                        <div>
+                            <img src={dashboard}></img>
+                            <span className={style.hide}> Dashboard </span>
+                        </div>
+                    </Tab.Panel>
+                    <Tab.Panel>
+                        {" "}
+                        <div className={style.active}>
+                            <img src={projects}></img>
+                            <span className={style.hide}>Projects</span>
+                        </div>
+                    </Tab.Panel>
+                    <Tab.Panel>
+                        <div>
+                            <img src={modules}></img>
+                            <span className={style.hide}>Modules </span>
+                        </div>
+                    </Tab.Panel>
+                    <Tab.Panel>
+                        {" "}
+                        <div>
+                            <img src={sprint}></img>
+                            <span className={style.hide}>Sprint </span>
+                        </div>
+                    </Tab.Panel>
+                    <Tab.Panel>
+                        <div>
+                            <img src={members}></img>
+                            <span className={style.hide}>Members </span>
+                        </div>
+                    </Tab.Panel>
+                    <Tab.Panel>
+                        {" "}
+                        <div>
+                            <img src={report}></img>
+                            <span className={style.hide}>Reports </span>
+                        </div>
+                    </Tab.Panel>
                 </Tab>
             </div>
             <Logout />
         </nav>
-    )
-}
+    );
+};
 
 export default Navbar;
